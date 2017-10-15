a8fb46e2d921deadf7ef8d79f56b674edaf7d65b
shreyansdjp <gamingeyefinity@gmail.com>
2017-10-16 01:13:31 +0530

    checking the routes to teach

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect
+from flask import Flask, render_template, flash, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return "hello"
 
-if __main__ == __name__:
+@app.route('/<string:name>')
+def goto(name):
+    return redirect("http://www.facebook.com", code=301)
+
+if __name__ == '__main__':
     app.run(debug=True)
